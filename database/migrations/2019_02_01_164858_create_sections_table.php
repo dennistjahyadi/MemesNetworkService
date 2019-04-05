4b6d0490823eba1ae360d7f1a80670ef9ca8599b
Dennis Tjahyadi <dennis.tgt@gmail.com>
2019-04-05 22:08:54 +0700

    as

--- database/migrations/2019_02_01_164858_create_sections_table.php
+++ database/migrations/2019_02_01_164858_create_sections_table.php
@@ -16,6 +16,7 @@ class CreateSectionsTable extends Migration
         Schema::create('sections', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
+            $table->string('images')->nullable();
             $table->timestamps();
         });
     }
